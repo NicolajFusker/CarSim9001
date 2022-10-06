2f90ea1ce074b3d07713fab51167dcbf41d68a3f
Fusker <nfr@nyraad.net>
2022-10-06 09:44:46 +0200

    Gearbox færdig, tror jeg

--- model.py
+++ model.py
@@ -11,12 +11,44 @@ class Wheel(object):
         degreesOfRotation = 360 * revolutions
         self.oriantation = (degreesOfRotation + self.oriantation) % 360
 
-
 class Engine(object):
     pass
 
 class Gearbox(object):
-    pass
+    def __init__(self):
+        self.wheels = {}
+        for newWheel in ['frontLeft', 'frontRight', 'rearLeft', 'rearRight']:
+            self.wheels[newWheel] = Wheel()
+
+        self.currentGear = 0
+        self.clutchEngaged = False
+        self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
+
+    def shiftUp(self):
+        if self.currentGear < len(self.gears) - 1 and not self.clutchEngaged:
+            self.currentGear += 1
+
+    def shiftDown(self):
+        if self.currentGear > 0 and not self.clutchEngaged:
+            self.currentGear -= 1
+
+    def rotate(self, revolutions):
+        if self.clutchEngaged:
+            newRevs = revolutions * self.gears[self.currentGear]
+            for wheel in self.wheels:
+                self.wheels[wheel].rotate(newRevs)
+
 
 class Tank(object):
-    pass
+    def __init__(self):
+        self.capacity = 100
+        self.contents = 100
+
+    def remove(self,amount):
+        self.contents -= amount
+        if self.contents < 0:
+            self.contents = 0
+
+    def refuel(self):
+        self.contents = self.capacity
+
