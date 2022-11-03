82141f525cd54fef95f7b04d1362ec4f0bb43003
Fusker <nfr@nyraad.net>
2022-11-03 19:35:18 +0100

    Kommentare

--- model.py
+++ model.py
@@ -1,22 +1,22 @@
-from random import randint
+from random import randint # Import random libary
 
-class Car(object):
-    def __init__(self):
+class Car(object): # Create car module
+    def __init__(self): # start engine
         self.theEngine = Engine()
 
-    def updateModel(self, dt):
+    def updateModel(self, dt): # Update the engine
         self.theEngine.updateModel(dt)
 
-class Wheel(object):
-    def __init__(self):
+class Wheel(object): # Create an recipie for the wheel
+    def __init__(self): # Give the wheel a random orientation
         self.orientation = randint(0, 359)
 
-    def rotate(self, revolutions):
+    def rotate(self, revolutions): # Make the wheel able to turn
         degreesOfRotation = 360 * revolutions
         self.orientation = (degreesOfRotation + self.orientation) % 360
 
-class Engine(object):
-    def __init__(self):
+class Engine(object): # Create the engine
+    def __init__(self): # Define variables for the engine
         self.throttlePosition = 0
         self.theGearbox = Gearbox()
         self.currentRpm = 0
@@ -24,7 +24,7 @@ class Engine(object):
         self.maxRpm = 100
         self.theTank = Tank()
 
-    def updateModel(self, dt):
+    def updateModel(self, dt):# Start the engine
         if self.theTank.contents > 0:
             self.currentRpm = self.throttlePosition * self.maxRpm
             self.theTank.remove(self.currentRpm * self.consumptionConstant)
@@ -32,8 +32,8 @@ class Engine(object):
         else:
             self.currentRpm = 0
 
-class Gearbox(object):
-    def __init__(self):
+class Gearbox(object): # Create a gearbox
+    def __init__(self): # Create four wheels
         self.wheels = {}
         for newWheel in ['frontLeft', 'frontRight', 'rearLeft', 'rearRight']:
             self.wheels[newWheel] = Wheel()
@@ -42,31 +42,31 @@ class Gearbox(object):
         self.clutchEngaged = False
         self.gears = [0, 0.8, 1, 1.4, 2.2, 3.8]
 
-    def shiftUp(self):
+    def shiftUp(self): # Shift up
         if self.currentGear < len(self.gears) - 1 and not self.clutchEngaged:
             self.currentGear += 1
 
-    def shiftDown(self):
+    def shiftDown(self): # shift down
         if self.currentGear > 0 and not self.clutchEngaged:
             self.currentGear -= 1
 
-    def rotate(self, revolutions):
+    def rotate(self, revolutions): # Turn the Wheels
         if self.clutchEngaged:
             newRevs = revolutions * self.gears[self.currentGear]
             for wheel in self.wheels:
                 self.wheels[wheel].rotate(newRevs)
 
 
-class Tank(object):
-    def __init__(self):
+class Tank(object): # Create a tank
+    def __init__(self): # Define variables for the tank, and fill the tank
         self.capacity = 100
         self.contents = 100
 
-    def remove(self,amount):
+    def remove(self,amount): # Remove fuel from the tank
         self.contents -= amount
         if self.contents < 0:
             self.contents = 0
 
-    def refuel(self):
+    def refuel(self): # Refuel the tank
         self.contents = self.capacity
 
